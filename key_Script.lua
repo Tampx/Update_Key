37dc6553328c5560f7c3557921091e0115c51733
Tampx <133935200+Tampx@users.noreply.github.com>
2025-05-11 22:41:27 +0700

    Update key_Script.lua

--- key_Script.lua
+++ key_Script.lua
@@ -1,6 +1,7 @@
 local Key_X = 'TamPxi'
 ZvwLP6L9hyLxLh = 'AGM'
 ZvwLP6L9hyLxLi = 'https://www.google.com'
+ZvwLP6L9hyLxLz = '11/05/2029'
 function loginInvoke(a)
 	if a[1] == Key_X then
 		return true
