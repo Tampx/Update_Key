80a0b562fbea882d6deb045a0621f67f025e7c22
Tampx <133935200+Tampx@users.noreply.github.com>
2025-05-15 23:17:42 +0700

    Update key_Script.lua

--- key_Script.lua
+++ key_Script.lua
@@ -1,15 +1,8 @@
 ZvwLP0L1hrLxLo = 'TamPxi'
 ZvwLP6L9hyLxLh = 'AGM'
 ZvwLP6L9hyLxLz = '11/05/2029'
-ZvwLP6L9hyLtLl = '1'
 --link key
---1
-ZvwLP6L9hyLaLi = 'https://www.google.com'
---2
-ZvwLP6L9hyLbLi = 'https://www.google.com'
---3
-ZvwLP6L9hyLcLi = 'https://www.google.com'
-
+ZvwLP6L9hyLxLi = 'https://www.google.com'
 function loginInvoke(a)
 	if a[1] == Key_X then
 		return true
